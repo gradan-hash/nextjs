e396844fe1e283761cd74d64029eca8413a7221c
gradan-hash <mutukunyaa@gmail.com>
2023-07-09 06:50:09 +0300

    wozza

---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Footer = () => {
+
+  console.log("eloo")
+  return (
+    <div>
+     <div>@ 2023 reserved.Cornelius</div>
+     <div></div>
+     </div>
+   
+  )
+}
+
+export default Footer
---
+++ src/components/navbar/Navbar.jsx
@@ -0,0 +1,53 @@
+import React from 'react'
+import Link from "next/link"
+
+
+
+const links = [
+  {
+    id:1,
+    title:"Home",
+    url: "/"
+  },
+  {
+    id:2,
+    title:"Portfolio",
+    url:"/portfolio"
+  },
+  {
+    id:3,
+    title:"blog",
+    url:"/blogg"
+  },
+  {
+    id:4,
+    title:"About",
+    url:"/about"
+  },
+  {
+    id:5,
+    title:"Contact",
+    url: "/contact"
+
+  },
+  {
+    id:6,
+    title:"Dashboard",
+    url:"/dashboard"
+  }
+
+]
+
+const Navbar = () => {
+
+
+  return (
+    <div>
+     {links.map((link)=>(
+      <Link key={link.id} href={link.url}>{link.title}</Link>
+     ))}
+    </div>
+  )
+}
+
+export default Navbar
