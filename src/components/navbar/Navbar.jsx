8ee529a04c9835266f1acb08339f7e3a841a0c4f
gradan-hash <mutukunyaa@gmail.com>
2023-07-10 05:38:08 +0300

    css

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,57 +1,63 @@
-"use client"
-import React from 'react'
-import Link from "next/link"
-
-
+"use client";
+import React from "react";
+import Link from "next/link";
+import styles from "./navbar/module.css";
 
 const links = [
   {
-    id:1,
-    title:"Home",
-    url: "/"
+    id: 1,
+    title: "Home",
+    url: "/",
   },
   {
-    id:2,
-    title:"Portfolio",
-    url:"/portfolio"
+    id: 2,
+    title: "Portfolio",
+    url: "/portfolio",
   },
   {
-    id:3,
-    title:"blog",
-    url:"/blogg"
+    id: 3,
+    title: "blog",
+    url: "/blogg",
   },
   {
-    id:4,
-    title:"About",
-    url:"/about"
+    id: 4,
+    title: "About",
+    url: "/about",
   },
   {
-    id:5,
-    title:"Contact",
-    url: "/contact"
-
+    id: 5,
+    title: "Contact",
+    url: "/contact",
   },
   {
-    id:6,
-    title:"Dashboard",
-    url:"/dashboard"
-  }
-
-]
+    id: 6,
+    title: "Dashboard",
+    url: "/dashboard",
+  },
+];
 
 const Navbar = () => {
-
-
   return (
-    <div>
-     {links.map((link)=>(
-      <Link key={link.id} href={link.url}>{link.title}</Link>
-     ))}
-     <button onClick ={()=>{
-      console.log("logged out")
-     }}>logout</button>
-    </div>
-  )
-}
+    <div className={styles.container}>
+      <Link href="/" className={styles.logo}>
+        cornelius
+      </Link>
+      <div className={styles.Links}>
+        {links.map((link) => (
+          <Link key={link.id} href={link.url} className={styles.link}>
+            {link.title}
+          </Link>
+        ))}
 
-export default Navbar
+        <button
+          onClick={() => {
+            console.log("logged out");
+          }}>
+          logout
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
